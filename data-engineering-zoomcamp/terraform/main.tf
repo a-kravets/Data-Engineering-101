292ecacc7b0b7d4c0ca83a85f4b097ab408a0ced
Andrew <a-kravets@users.noreply.github.com>
2023-12-08 16:10:19 +0100

    init commit

---
+++ data-engineering-zoomcamp/terraform/main.tf
@@ -0,0 +1,41 @@
+# https://registry.terraform.io/providers/hashicorp/google/latest/docs
+
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "5.8.0"
+    }
+  }
+}
+
+
+provider "google" {
+  credentials = file(var.credentials)
+  project     = var.project
+  region      = var.region
+}
+
+
+resource "google_storage_bucket" "demo-bucket" {
+  name          = "${local.data_lake_bucket}_${var.project}"
+  location      = var.location
+  force_destroy = true
+
+
+  lifecycle_rule {
+    condition {
+      age = 1
+    }
+    action {
+      type = "AbortIncompleteMultipartUpload"
+    }
+  }
+}
+
+
+
+resource "google_bigquery_dataset" "demo_dataset" {
+  dataset_id = var.bq_dataset_name
+  location   = var.location
+}
