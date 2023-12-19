04244b0de6e52ac53ec9d8f4363934ad3e43924c
Andrew <a-kravets@users.noreply.github.com>
2023-12-19 13:31:24 +0100

    adding ml model

--- data-engineering-zoomcamp/03_bigquery/big_query_ml.sql
+++ data-engineering-zoomcamp/03_bigquery/big_query_ml.sql
@@ -23,3 +23,90 @@ CREATE OR REPLACE TABLE
     `de-zoomcamp-407509.demo_dataset.external_yellow_tripdata_3m`
   WHERE
     fare_amount != 0);
+
+
+-- CREATE MODEL WITH DEFAULT SETTING
+CREATE OR REPLACE MODEL `de-zoomcamp-407509.demo_dataset.tip_model`
+OPTIONS
+(model_type='linear_reg',
+-- input_label_cols is what we want to predict, i.e. tip amount
+input_label_cols=['tip_amount'],
+-- will split the data into training & evaluation sets
+DATA_SPLIT_METHOD='AUTO_SPLIT') AS
+SELECT
+  *
+FROM
+  `de-zoomcamp-407509.demo_dataset.yellow_tripdata_ml`
+WHERE
+tip_amount IS NOT NULL;
+
+
+-- CHECK FEATURES
+SELECT * FROM ML.FEATURE_INFO(MODEL `de-zoomcamp-407509.demo_dataset.tip_model`);
+
+
+-- EVALUATE THE MODEL
+SELECT
+  *
+FROM
+  ML.EVALUATE(MODEL `de-zoomcamp-407509.demo_dataset.tip_model`,
+  (
+    SELECT
+      *
+    FROM
+      `de-zoomcamp-407509.demo_dataset.yellow_tripdata_ml`
+    WHERE
+      tip_amount IS NOT NULL
+    ));
+
+
+
+-- PREDICT THE MODEL
+SELECT
+  *
+FROM
+  ML.PREDICT(MODEL `de-zoomcamp-407509.demo_dataset.tip_model`,
+  (
+  SELECT
+    *
+  FROM
+    `de-zoomcamp-407509.demo_dataset.yellow_tripdata_ml`
+  WHERE
+    tip_amount IS NOT NULL
+  ));
+
+
+  -- PREDICT AND EXPLAIN
+SELECT
+  *
+FROM
+  ML.EXPLAIN_PREDICT(MODEL `de-zoomcamp-407509.demo_dataset.tip_model`,
+    (
+    SELECT
+      *
+    FROM
+      `de-zoomcamp-407509.demo_dataset.yellow_tripdata_ml`
+    WHERE
+      tip_amount IS NOT NULL ),
+    -- sets to show 3 features that had the biggest impact 
+    STRUCT(3 AS top_k_features));
+
+
+  -- https://cloud.google.com/bigquery/docs/reference/standard-sql/bigqueryml-syntax-create
+  -- https://cloud.google.com/bigquery/docs/create-machine-learning-model
+  -- HYPER PARAM TUNNING
+CREATE OR REPLACE MODEL
+  `de-zoomcamp-407509.demo_dataset.tip_model` OPTIONS (model_type='linear_reg',
+    input_label_cols=['tip_amount'],
+    DATA_SPLIT_METHOD='AUTO_SPLIT',
+    num_trials=5,
+    max_parallel_trials=2,
+    l1_reg=hparam_range(0, 20),
+    l2_reg=hparam_candidates([0, 0.1, 1, 10])
+    ) AS
+SELECT
+  *
+FROM
+  `de-zoomcamp-407509.demo_dataset.yellow_tripdata_ml`
+WHERE
+  tip_amount IS NOT NULL;
